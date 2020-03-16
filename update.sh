ba24988b3f49c98770dbaf47e44b54bba132d021
rick <rick@ricks-MacBook-Air.local>
2020-03-16 16:06:11 -0400

    m

--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 set -e
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-_F=$(date +%s).csv
+_TS="$(date -d "$(date +%Y%m%d) $(date +%H):00:00" +%s)"
+
+_F=${_TS}.csv
 curl -s https://www.cdc.gov/coronavirus/2019-ncov/map-data-cases.csv -o $_F
 git add $_F
 git commit $_F -m 'auto update'
